11e0d347f5fa1b10e6602e16e5c96bef171e2e46
SornchaiTheDev <SornchaiTheDev@gmail.com>
2019-12-14 23:57:11 +0700

    admin

--- Doctor/index.php
+++ Doctor/index.php
@@ -5,19 +5,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="node_modules/sweetalert2/dist/sweetalert2.min.css">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="../node_modules/sweetalert2/dist/sweetalert2.min.css">
     <title>Document</title>
 </head>
 
 <body>
 
 
-    <script src=" node_modules/jquery/dist/jquery.min.js"> </script>
-    <script src="css/fontawesome/js/all.min.js"></script>
-    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
+    <!--Welcome Message-->
+    <div class="container mt-2">
+        <marquee behavior="" direction="">ประกาศ : Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ratione quo modi dicta maiores sapiente fugiat deleniti delectus officia, odio asperiores, quisquam nostrum, rerum hic aperiam porro consequuntur illo cum!</marquee>
+
+        <div class="" style="border : 5px solid; border-radius : 50px">
+            <h1 class="text-center" style="margin-top : 100px; margin-bottom : 100px">DoctorAssistant</h1>
+            <button class="btn btn-primary d-block mx-auto mb-4" style="font-size : 50px;">ประวัติผู้ป่วย</button>
+            <button class="btn btn-dark d-block mx-auto" style="font-size : 50px;">ลงทะเบียนผู้ป่วยใหม่</button>
+            <div style="margin-top : 300px;">
+            </div>
+        </div>
+
+        <!--New Patient-->
+        <div class="container">
+
+
+        </div>
+
+
+
+
+
+        <script src="../node_modules/jquery/dist/jquery.min.js"> </script>
+        <script src="../css/fontawesome/js/all.min.js"></script>
+        <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="../node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
 </body>
 
 </html>
