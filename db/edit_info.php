29b5f98e792306c8b8ee40230a675865bf365092
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2021-11-30 15:18:52 +0700

    Update edit_info.php

--- db/edit_info.php
+++ db/edit_info.php
@@ -6,16 +6,16 @@ if (isset($_SESSION['qr'])) {
   $qr_id = $_SESSION['qr'];
   $telephone = $_POST['telephone'];
   $home = $_POST['home'];
+  $stmt = $conn->prepare("UPDATE info SET home = ? , telephone = ? WHERE qr_id = ?");
+  $stmt->bind_param("sii" ,$home,$telephone,$qr_id);
+  $stmt->execute();
+  $status = $stmt->get_result();
 
-  $status = $conn->query("UPDATE info SET home = '$home' , telephone = '$telephone' WHERE qr_id = '$qr_id'");
-
-  if (!$status) {
-    echo "error";
-  } else {
+  if ($stmt->affected_rows > 0) {
     echo "<p class='text-success'>บันทึกข้อมูลสำเร็จ</p>";
+  } else {
+    echo "<p class='text-danger'>บันทึกข้อมูลไม่สำเร็จ</p>";
   }
 }else{
   echo "<p class='text-danger'>บันทึกข้อมูลไม่สำเร็จ กรุณาเข้าสู่ระบบอีกครั้ง</p>";
 }
-
-
