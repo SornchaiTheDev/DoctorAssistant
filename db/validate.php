cf989c7d6c180db11a58e0b56075b5b00ed6b018
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2021-11-30 15:18:27 +0700

    Update validate.php

--- db/validate.php
+++ db/validate.php
@@ -3,15 +3,19 @@ require "connect.php";
 
 $id_card = $_POST['id_card'];
 $qrcode = $_POST['qrcode'];
-$result = $conn->query("SELECT * FROM Users WHERE id_card  = '$id_card' and qr_id = '$qrcode'")->fetch_assoc();
-if(!$result){
-    echo "<p class='text-danger'>*เลขบัตรประจำตัวประชาชนผิด<p>";
+
+$stmt = $conn->prepare("SELECT * FROM Users WHERE id_card  = ? and qr_id = ?");
+$stmt->bind_param("ii", $id_card , $qrcode);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if($result->num_rows > 0){
+     echo '
+     <script>
+     window.location.href = "home.php"
+     </script>
+     ';
 }
 else {
- echo '
- <script>
-  window.location.href = "home.php"
- </script>
- ';
+     echo "<p class='text-danger'>*เลขบัตรประจำตัวประชาชนผิด<p>";
 }
-
